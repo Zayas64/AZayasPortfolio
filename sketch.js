c17845f800f453e5e23eb51046f248609f82ba0e
Zayas64 <57229623+Zayas64@users.noreply.github.com>
2019-11-21 11:56:24 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+    stroke(255, 0, 0);
+    strokeWhight(3);
+        arc(150, 100, 110, 50, 0, PI / 1.0);
+        nofill();
+}
